7c949e390f525fd79770810e2f70c7f393d46812
Dani Riera <daniel.riera@qatium.com>
2025-02-23 23:07:50 +0100

    Update the styles
    Add a bg image

--- src/components/IpData/IpData.tsx
+++ src/components/IpData/IpData.tsx
@@ -1,31 +1,36 @@
 import { FunctionComponent } from "react";
 import { useIpData } from "./useIpData";
+import styles from "./IpData.module.css";
 
 export const IpData: FunctionComponent = () => {
-  const ipData = useIpData();
-  const { ip, location } = ipData.data;
+  const {
+    data: { ip, location },
+    loading
+  } = useIpData();
   return (
     <>
-      {ipData.loading && (
+      {loading && (
         <div role="alert" aria-busy="true">
           Loading your IP address...
         </div>
       )}
 
-      {ip && (
-        <div>
-          <h2>Your IP Address</h2>
-          <p>{ip}</p>
-        </div>
-      )}
-      {location && (
-        <div>
-          <h2>Location</h2>
-          <p>
-            {location.city}, {location.region}, {location.country}
-          </p>
-        </div>
-      )}
+      <div className={styles.dataBoxes}>
+        {ip && (
+          <div className={styles.box}>
+            <h2 className={styles.boxTitle}>Your IP Address</h2>
+            <p className={styles.boxText}>{ip}</p>
+          </div>
+        )}
+        {location && (
+          <div className={styles.box}>
+            <h2 className={styles.boxTitle}>Location</h2>
+            <p className={styles.boxText}>
+              {location.city}, {location.region}, {location.country}
+            </p>
+          </div>
+        )}
+      </div>
     </>
   );
 };
